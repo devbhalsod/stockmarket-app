27a65b361f2168ed51c6c28ba9aa96de66b1ab8e
devbhalsod <devbhalsod777@gmail.com>
2025-12-04 15:47:47 +0530

    Impelment home page with tradingView widgets

---
+++ components/TradingViewWidget.tsx
@@ -0,0 +1,36 @@
+// TradingViewWidget.jsx
+"use client"
+import React, {  useRef, memo } from 'react';
+import useTradingViewWidget from './hooks/useTradingViewWidget';
+import { Heading3 } from 'lucide-react';
+import { cn } from '@/lib/utils';
+
+interface TradingViewWidgetProps {
+    title?: string;
+    scriptUrl: string;
+    config: Record<string, unknown>;
+    height?: number;
+    className?: string;
+}
+
+const TradingViewWidget = ({
+    title,scriptUrl,config,height=600,className
+}:TradingViewWidgetProps) => {
+  const containerRef = useTradingViewWidget(scriptUrl,config,height);
+
+  
+
+  return (
+
+    <div className='w-full'>
+        {title && <h3 className='font-semibold text-gray-100 mb-5'>{title}</h3>}
+
+    <div className= {cn("tradingview-widget-container",className)} ref={containerRef} >
+      <div className="tradingview-widget-container__widget" style={{ height: height, width: "100%" }}></div>
+     
+    </div>
+    </div>
+  );
+}
+
+export default memo(TradingViewWidget);
